ad7152d9fed05fab86a1878d3e8e2fd05112ba16
Cenk Atahan Özbek <atahanozzbek@gmail.com>
2022-04-05 12:56:57 +0300

    fix

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-window.onload = function(){
-    var buttonSubmit = document.getElementById("buttton-submit")
-    var textInput = document.getElementById("input-text")
-}
-
+var buttonSubmit = document.getElementById("buttton-submit")
+var textInput = document.getElementById("input-text")
 
 //get text
 //add to list
 function print(){
-    console.log(textInput.value)
+    alert(textInput.value)
 }
